6ec5e86a361cfd382a6772d569631fa5de91f831
Fausto <fausto91@gmail.com>
2019-11-03 12:17:41 +0100

    Fix utilizzo webapp in locale

--- src/main/webapp/app/app.constants.ts
+++ src/main/webapp/app/app.constants.ts
@@ -4,5 +4,6 @@
 
 export const VERSION = process.env.VERSION;
 export const DEBUG_INFO_ENABLED: boolean = !!process.env.DEBUG_INFO_ENABLED;
-export const SERVER_API_URL = process.env.SERVER_API_URL;
+export const SERVER_API_URL =
+    document.location.hostname === 'localhost' ? `${document.location.protocol}//${document.location.host}/` : process.env.SERVER_API_URL;
 export const BUILD_TIMESTAMP = process.env.BUILD_TIMESTAMP;
