f6db059a04d66845f80565bcfe87101fc9897204
Fausto <fausto91@gmail.com>
2019-11-03 12:17:11 +0100

    Nuova direttiva angular hasNotAuthority

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -3,13 +3,14 @@ import { NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 
 import { NgbDateMomentAdapter } from './util/datepicker-adapter';
 import { ApigatewaySharedLibsModule, ApigatewaySharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective } from './';
+import { HasNotAuthorityDirective } from 'app/shared/auth/has-not-authority.directive';
 
 @NgModule({
     imports: [ApigatewaySharedLibsModule, ApigatewaySharedCommonModule],
-    declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective],
+    declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective, HasNotAuthorityDirective],
     providers: [{ provide: NgbDateAdapter, useClass: NgbDateMomentAdapter }],
     entryComponents: [JhiLoginModalComponent],
-    exports: [ApigatewaySharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective],
+    exports: [ApigatewaySharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective, HasNotAuthorityDirective],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ApigatewaySharedModule {
